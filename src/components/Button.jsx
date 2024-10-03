c37277c6a396ec3c07c87ed466ab58e428d7f229
Gabo <gabe.anzola@gmail.com>
2024-10-03 17:36:22 -0300

    Footer

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,30 @@
-const Button = ({ label, iconURL }) => {
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  textColor,
+  borderColor,
+  fullWidth,
+}) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-red-600 rounded-full text-white hover:animate-bounce">
-    {label}
-    <img src={iconURL} alt="arrow right" className="ml-2 rounded-full w-5 h-5" />
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none hover:animate-bounce 
+        ${
+          backgroundColor
+            ? `${backgroundColor} ${textColor} ${borderColor}`
+            : "text-white bg-red-600"
+        } rounded-full ${fullWidth && 'w-full'} `}
+    >
+      {label}
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="arrow right"
+          className="ml-2 rounded-full w-5 h-5"
+        />
+      )}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
